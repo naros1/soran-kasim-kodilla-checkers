ec3957a2e77c30dd785a38d84eadd70424c61ca1
naros1 <sorankasim@gmail.com>
2019-08-01 20:22:41 +0200

    first commit

---
+++ src/main/java/com/kodilla/kodillacoursecheckers/KodillaCourseCheckersApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacoursecheckers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourseCheckersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourseCheckersApplication.class, args);
+    }
+
+}
